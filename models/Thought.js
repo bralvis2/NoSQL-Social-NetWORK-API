6ad8522f8926e5222a99764cbaea5c73464a754c
Brenda Alvis <brendaalvis@brendas-mbp-2.lan>
2022-09-10 21:01:52 -0500

    Fixed thought model removed reaction model

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,41 @@
 const { Schema, model } = require("mongoose");
 
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+
+        username: {
+            type: String,
+            required: true,
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            get: (date) => {
+                if (date) return date.toISOString().split('T')[0];
+            },
+        },
+    },
+    {
+        // timestamps: true,
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
 const thoughtSchema = new Schema(
     {
         thoughtText: {
@@ -12,11 +48,35 @@ const thoughtSchema = new Schema(
         createdAt: {
             type: Date,
             default: Date.now,
+            get: (date) => {
+                if (date) return date.toISOString().split('T')[0];
+            },
         },
 
         username: {
             type: String,
             required: true,
         },
+        reactions: {
+            type: [reactionSchema],
+        },
+    },
+    {
+    
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: true,
     }
-)
+);
+
+thoughtSchema 
+    .virtual('reactionCount')
+    // Getter method to format the timestamp on query.
+    .get(function (){
+        return this.reactions.length;
+    });
+
+const Thought = model('thought', thoughtSchema);
+model.exports = Thought;
