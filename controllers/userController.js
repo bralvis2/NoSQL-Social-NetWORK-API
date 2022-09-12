79c35421b0c74cdc344c35aaa5b01b37678a4f74
Brenda Alvis <brendaalvis@brendas-mbp-2.lan>
2022-09-12 17:25:21 -0500

    adding and delteing friendsfrom friend's list

--- controllers/userController.js
+++ controllers/userController.js
@@ -56,4 +56,39 @@ createUser(req, res) {
       .catch((err) => res.status(500).json(err));
   },
 
+  // Add a new friend to user's friend list
+  addFriend(req, res) {
+    console.log("You've added a new friend to your friend's list");
+    console.log(req.body);
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: 'No user found with that ID :(' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Delete a friend from friend's list
+  removeFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: { friendId: req.params.friendId } } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res
+              .status(404)
+              .json({ message: 'No user found with that ID :(' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 }
