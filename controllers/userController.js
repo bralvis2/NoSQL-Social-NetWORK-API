c6211951a7fbbc2983a0bd1a3e4da91ecd4e5a09
Brenda Alvis <brendaalvis@brendas-mbp-2.lan>
2022-09-12 17:13:11 -0500

    Functions for creating, updating, and deleting users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,14 @@
 const { User, Thought } = require('../models');
 
 module.exports = {
+    // Aggregate function to get all Users
     getUsers(req, res) {
         User.find()
             .then((users) => res.json(users))
             .catch((err) => res.status(500).json(err));
     },
 
+    // Aggregate function to get a single user
     getUserId(req, res) {
         User.findOne({_id: req.params.userId})
         .select('-__v')
@@ -16,5 +18,42 @@ module.exports = {
         : res.json(user)
     )
     .catch((err) => res.status(500).json(err));
-}
+},
+// Create a new user
+createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+
+  // Update User 
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user found with this id!' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Delete User 
+  deleteUser(req, res) {
+   User.findOneAndDelete({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user found with that ID' })
+          : Thought.deleteMany({ _id: { $in: user.thoughts } })
+      )
+      .then(() => res.json({ message: 'User and thoughts have been deleted!' }))
+      .catch((err) => res.status(500).json(err));
+  },
+
 }
