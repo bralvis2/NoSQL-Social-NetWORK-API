2759bbd1aef1acdce838aa85011c173c0d9dbdd5
Brenda Alvis <brendaalvis@brendas-mbp-2.lan>
2022-09-12 20:42:11 -0500

    Generate functions to use as seeds

---
+++ utils/data.js
@@ -0,0 +1,60 @@
+const names = [
+    'Rhaenyra',
+    'Daemon',
+    'Viserys',
+    'Daenerys',
+    'Jon',
+    'Arya',
+    'Cersei',
+];
+
+const appDescriptions = [
+    'Winter is coming',
+    'There is only one thing we say to death: Not today.',
+    'The north remembers',
+    'Yes. All men must die, but we are not men.',
+    'Tell Cersei. I want her to know it was me.',
+    'You  know nothing, Jon Snow',
+    'When you play the game of thrones, you win or you die. There is no middle ground',
+];
+
+const reactions = [
+    'Like',
+    'Heart',
+    'Dislike',
+    'Angry',
+    'Sad',
+    'Wow',
+]
+// Get a random item given an array
+const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+// Gets a random name
+const getRandomName = () =>
+    `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
+
+// Function to generate random assignments that we can add to student object.
+const getRandomThoughts = (int) => {
+    const results = [];
+    for (let i = 0; i < int; i++) {
+        results.push({
+            thoughtText: getRandomArrItem(appDescriptions),
+            username: getRandomName().split(' ')[0],
+        });
+    }
+    return results;
+};
+
+// Function to generate random reactions that we can add to thought object.
+const getRandomReactions = (int) => {
+    const results = [];
+    for (let i = 0; i < int; i++) {
+        results.push({
+            reactionBody: getRandomArrItem(reactions),
+        });
+    }
+    return results;
+};
+
+// Export the functions for use in seed.js
+module.exports = { getRandomName, getRandomThoughts, getRandomReactions };
